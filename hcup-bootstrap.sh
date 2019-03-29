cf95023d530e68aadea68ed78de7d74451162d4d
neonphog <neonphog@gmail.com>
2019-03-28 18:52:32 -0600

    simplify linux distro detect

--- hcup-bootstrap.sh
+++ hcup-bootstrap.sh
@@ -152,8 +152,8 @@ var hcup_bootstrap = (function (exports) {
 	};
 	});
 
-	var debian = createCommonjsModule(function (module, exports) {
-	// check to see if we are debian
+	var osRelease = createCommonjsModule(function (module, exports) {
+	// check to see if we are an os that can be identified by /etc/os-release
 
 	const fs = require('fs');
 
@@ -164,72 +164,27 @@ var hcup_bootstrap = (function (exports) {
 	    }
 
 	    const res = fs.readFileSync('/etc/os-release', 'utf8');
+
 	    if (/ID=debian/m.test(res)) {
 	      env.distro = 'debian';
 	      env.selector.push('debian');
-	      const m = res.match(/VERSION_ID="([^"]+)"/m);
-	      if (m && m.length >= 2) {
-	        env.selector.push(m[1]);
-	        env.distro_version = m[1];
-	      }
-	    }
-	  } catch (e) {
-	    console.error(e);
-	  }
-	};
-	});
-
-	var ubuntu = createCommonjsModule(function (module, exports) {
-	// check to see if we are debian
-
-	const fs = require('fs');
-
-	module.exports = exports = env => {
-	  try {
-	    if (env.selector.length > 1) {
-	      return
-	    }
-
-	    const res = fs.readFileSync('/etc/os-release', 'utf8');
-	    if (/ID=ubuntu/m.test(res)) {
+	    } else if (/ID=ubuntu/m.test(res)) {
 	      env.distro = 'ubuntu';
 	      env.selector.push('ubuntu');
-	      const m = res.match(/VERSION_ID="?([^"\s]+)"?/m);
-	      if (m && m.length >= 2) {
-	        env.selector.push(m[1]);
-	        env.distro_version = m[1];
-	      }
-	    }
-	  } catch (e) {
-	    console.error(e);
-	  }
-	};
-	});
-
-	var fedora = createCommonjsModule(function (module, exports) {
-	// check to see if we are debian
-
-	const fs = require('fs');
-
-	module.exports = exports = env => {
-	  try {
-	    if (env.selector.length > 1) {
+	    } else if (/ID=fedora/m.test(res)) {
+	      env.distro = 'fedora';
+	      env.selector.push('fedora');
+	    } else {
+	      // could not identify
 	      return
 	    }
 
-	    const res = fs.readFileSync('/etc/os-release', 'utf8');
-	    if (/ID=fedora/m.test(res)) {
-	      env.distro = 'fedora';
-	      env.selector.push('fedora');
-	      const m = res.match(/VERSION_ID="?([^"\s]+)"?/m);
-	      if (m && m.length >= 2) {
-	        env.selector.push(m[1]);
-	        env.distro_version = m[1];
-	      }
+	    const m = res.match(/VERSION_ID="?([^"\s]+)"?/m);
+	    if (m && m.length >= 2) {
+	      env.selector.push(m[1]);
+	      env.distro_version = m[1];
 	    }
-	  } catch (e) {
-	    console.error(e);
-	  }
+	  } catch (e) { /* pass */ }
 	};
 	});
 
@@ -265,9 +220,7 @@ var hcup_bootstrap = (function (exports) {
 	}
 
 	nix(exports);
-	debian(exports);
-	ubuntu(exports);
-	fedora(exports);
+	osRelease(exports);
 
 	let isVerbose = false;
 	exports.setVerbose = () => { isVerbose = true; };
